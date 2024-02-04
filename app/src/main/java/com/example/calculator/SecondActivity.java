a1fe79c879efeee42564aaf529345b533ebf7e55
ad1ru <adiru04@gmail.com>
2024-02-04 17:28:20 +0600

    working dropdown menu and range sliders

--- app/src/main/java/com/example/calculator/SecondActivity.java
+++ app/src/main/java/com/example/calculator/SecondActivity.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class SecondActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SecondActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,34 @@ public class SecondActivity extends AppCompatActivity {
         String data = getIntent().getStringExtra("key1");
         TextView textView = findViewById(R.id.text_view);
         textView.setText(data);
+
+        final Spinner spinner = (Spinner) findViewById(R.id.dropdown_menu);
+
+        spinner.setOnItemSelectedListener(this);
+
+        List<String> categories = new ArrayList<String>();
+        categories.add("Item 1");
+        categories.add("Item 2");
+        categories.add("Item 3");
+        categories.add("Item 4");
+        categories.add("Item 5");
+        categories.add("Item 6");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categories);
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        spinner.setAdapter(dataAdapter);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String item = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), "Selected " + item, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+
     }
 
     public void onNextClick(View view) {
